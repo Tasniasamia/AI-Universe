034f371e34081dd806bff75d2b4f2a4a8ca08a23
Tasnia <tasniaarhamarham@gmail.com>
2023-03-03 02:59:40 +0600

    modal_resizing

--- script.js
+++ script.js
@@ -62,10 +62,10 @@ document.getElementById('modal_body').innerHTML=`
 
 <div class="row row-cols-1 row-cols-md-2 g-4">
 <div class="col">
-  <div class="card">
+  <div class="card"">
    
     <div class="card-body">
-     
+    
     </div>
   </div>
 </div>
